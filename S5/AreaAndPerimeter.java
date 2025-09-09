384e83fee8707be780b8f62e432f6629c5973bc1
abelgeorgeantony <abelgeorgeantony@gmail.com>
2025-09-09 16:44:05 +0530

    Completed S5/Fibonacci.java

--- S5/AreaAndPerimeter.java
+++ S5/AreaAndPerimeter.java
@@ -1,66 +1,73 @@
 import java.io.*;
-import java.util.sc;
+import java.util.Scanner;
+
 class Area {
-  int area;
-  public Area(int side) {
-    area=side*side;
-  }
-  public Area(int len, int bre) {
-    area=len*bre;
-  }
+
+    int area;
+
+    public Area(int side) {
+        area = side * side;
+    }
+
+    public Area(int len, int bre) {
+        area = len * bre;
+    }
 }
+
 class Perimeter {
-  int perimeter;
-  public Perimeter(int side) {
-    perimeter=4*side;
-  }
-  public Perimeter(int len, int bre) {
-    perimeter=(2*(len+bre));
-  }
+
+    int perimeter;
+
+    public Perimeter(int side) {
+        perimeter = 4 * side;
+    }
+
+    public Perimeter(int len, int bre) {
+        perimeter = (2 * (len + bre));
+    }
 }
+
 public class AreaAndPerimeter {
-  public static void main(String[] args) {
-    sc sc = new sc(System.in);
-    System.out.println("Enter choice: ");
-    System.out.println("1) Area:\n2) Perimeter\n");
-    int choice = sc.nextInt();
-    if(choice == 1) {
-      System.out.println("Select shape: ");
-      System.out.println("1) Square:\n2) Rectangle\n");
-      choice = sc.nextInt();
-      if(choice == 1) {
-        System.out.println("Enter length of side:");
-        int side = sc.nextInt();
-        Area aobj = new Area(side);
-        System.out.println("Area of Square: " + aobj.area);
-      }
-      else if(choice == 2) {
-        System.out.println("Enter length and bredth:");
-        int len = sc.nextInt();
-        int bre = sc.nextInt();
-        Area aobj = new Area(len, bre);
-        System.out.println("Area of Rectangle: " + aobj.area);
-      }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter choice: ");
+        System.out.println("1) Area:\n2) Perimeter\n");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            System.out.println("Select shape: ");
+            System.out.println("1) Square:\n2) Rectangle\n");
+            choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("Enter length of side:");
+                int side = sc.nextInt();
+                Area aobj = new Area(side);
+                System.out.println("Area of Square: " + aobj.area);
+            } else if (choice == 2) {
+                System.out.println("Enter length and bredth:");
+                int len = sc.nextInt();
+                int bre = sc.nextInt();
+                Area aobj = new Area(len, bre);
+                System.out.println("Area of Rectangle: " + aobj.area);
+            }
+        } else if (choice == 2) {
+            System.out.println("Select shape: ");
+            System.out.println("1) Square:\n2) Rectangle\n");
+            choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("Enter length of side:");
+                int side = sc.nextInt();
+                Perimeter pobj = new Perimeter(side);
+                System.out.println("Perimeter of Square: " + pobj.perimeter);
+            } else if (choice == 2) {
+                System.out.println("Enter length and bredth:");
+                int len = sc.nextInt();
+                int bre = sc.nextInt();
+                Perimeter pobj = new Perimeter(len, bre);
+                System.out.println("Perimeter of Rectangle: " + pobj.perimeter);
+            }
+        }
+
+        sc.close();
     }
-    else if(choice == 2) {
-      System.out.println("Select shape: ");
-      System.out.println("1) Square:\n2) Rectangle\n");
-      choice = sc.nextInt();
-      if(choice == 1) {
-        System.out.println("Enter length of side:");
-        int side = sc.nextInt();
-        Perimeter pobj = new Perimeter(side);
-        System.out.println("Perimeter of Square: " + pobj.perimeter);
-      }
-      else if(choice == 2) {
-        System.out.println("Enter length and bredth:");
-        int len = sc.nextInt();
-        int bre = sc.nextInt();
-        Perimeter pobj = new Perimeter(len, bre);
-        System.out.println("Perimeter of Rectangle: " + pobj.perimeter);
-      }
-    }
-    
-    sc.close();
-  }
 }
---
+++ S5/Fibonacci.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Generator {
+
+    public void generate(int limit) {
+        int n1 = 0,
+            n2 = 1;
+
+        for (int i = 1; i <= limit; ++i) {
+            System.out.print(n1 + " ");
+
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+        System.out.println();
+    }
+}
+
+public class Fibonacci {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        System.out.print("Enter the limit for the Fibonacci series: ");
+        String input = reader.readLine();
+        int limit = Integer.parseInt(input);
+
+        Generator fibGenerator = new Generator();
+        fibGenerator.generate(limit);
+    }
+}
